ed0b6fb8401f81e9848967c671e7f92ee6ff1cfa
pratiksd <pratik7d@gmail.com>
2026-07-03 11:35:37 +0530

    removed unwanted code

--- pharmacy-ui/src/services/medicineService.js
+++ pharmacy-ui/src/services/medicineService.js
@@ -3,14 +3,6 @@ import api from "./api";
 export const getMedicines = () =>
     api.get("/pharmacy");
 
-export const getMedicine = (id) =>
-    api.get(`/pharmacy/${id}`);
-
 export const addMedicine = (medicine) =>
     api.post("/pharmacy", medicine);
 
-export const updateMedicine = (id, medicine) =>
-    api.put(`/pharmacy/${id}`, medicine);
-
-export const deleteMedicine = (id) =>
-    api.delete(`/pharmacy/${id}`);
